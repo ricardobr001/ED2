3b28f9a505694f9a923e4fa6eae03422c8605c02
ricardobr001 <ricardomlealjr@gmail.com>
2016-10-12 15:49:42 -0300

    Funções de verificação implementadas

--- Trabalho2/562262_ED2_T02.c
+++ Trabalho2/562262_ED2_T02.c
@@ -5,8 +5,8 @@
  *
  * Trabalho 02 - Árvore B
  *
- * RA:
- * Aluno:
+ * RA: 562262
+ * Aluno: Ricardo Mendes Leal Junior
  * ========================================================================== */
 
 
@@ -110,6 +110,42 @@ void exibir_registro(int rrn);
 
 /* <<< DECLARE AQUI OS PROTOTIPOS >>> */
 
+/*Verifica se os caracteres do nome é válido*/
+int verificaNome(char *nome);
+
+/*Verifica se os caracteres do tipo são válidos*/
+int verificaTipoPokemon(char *tipo);
+
+/*Verifica se os caracteres do CP é válido*/
+int verificaCP(char *cp);
+
+/*Verifica se os caracteres da data são válidos*/
+int verificaData(char *data);
+
+/*Verifica o mês*/
+int verificaMes(char *data);
+
+/*Verifica se o ano é bissexto*/
+int verificaBissexto(char *data);
+
+/*Verifica se os caracteres da hora são válidos*/
+int verificaHora(char *hora);
+
+/*Verifica se o nome do treinador é válido*/
+int verificaTreinador(char *treinador);
+
+/*Verifica se o nivel do treinador é válido*/
+int verificaNivel(char *nivel);
+
+/*Verifica se o nome da equipe é válido*/
+int verificaEquipe(char *equipe);
+
+/*Função que gera a chave primária de um novo registro*/
+void geraChavePrimaria(Pokemon *info);
+
+/*Função que verifica se a chave primaria gerada já existe*/
+int verificaChave(Pokemon info, Indice *vet, int tam);
+
 /* ==========================================================================
  * ============================ FUNÇÃO PRINCIPAL ============================
  * =============================== NÃO ALTERAR ============================== */
@@ -198,6 +234,392 @@ void ignore() {
 }
 
 /* <<< IMPLEMENTE AQUI AS FUNCOES AUXILIARES >>> */
+
+/*Verifica se os caracteres do nome é válido*/
+int verificaNome(char *nome)
+{
+    int i = 0;
+
+    while (nome[i] != '\0')		//Andando o string até encontrar o \0
+    {
+        if ((nome[i] < 'A' || nome[i] > 'Z') && (nome[i] < 'a' || nome[i] > 'z'))		//Se algum caractere não for uma letra, o nome é inválido
+        {
+            return 0;
+        }
+
+        i++;
+    }
+
+    i = 0;
+
+    while (nome[i] != '\0')		//Deixando todos os caracteres maiusculos
+    {
+        nome[i] = toupper(nome[i]);
+        i++;
+    }
+
+    return 1;
+}
+
+/*Verifica se os caracteres do tipo são válidos*/
+int verificaTipoPokemon(char *tipo)
+{
+    int i = 0, flag = 0;
+
+    while (tipo[i] != '\0')		//Andando a string até encontrar o \0
+    {
+        if (((tipo[i] < 'A' || tipo[i] > 'Z') && (tipo[i] < 'a' || tipo[i] > 'z')) && tipo[i] != '/')		//Se algum caractere não for letra e for diferente da barra, o tipo é inválido
+        {
+            return 0;
+        }
+
+        if (tipo[i] == '/')		//Se o caractere for /, entra no if
+        {
+            if (i == 0)		//Se a barra estiver na primeira posição, o tipo é inválido
+            {
+                return 0;
+            }
+            else if (tipo[i+1] == '\0')		//Se a barra estiver na ultima posição, o tipo é inválido
+            {
+                return 0;
+            }
+            
+            flag++;		//Somando no contador que encontrou uma barra
+        }
+        
+        if (flag == 2)		//Se tiver duas barras, o tipo é inválido
+        {
+            return 0;
+        }
+
+        i++;
+    }
+
+    i = 0;
+
+    while (tipo[i] != '\0')		//Deixando todos os caracteres maiusculos
+    {
+        tipo[i] = toupper(tipo[i]);
+        i++;
+    }
+
+    return 1;
+}
+
+/*Verifica se os caracteres do CP é válido*/
+int verificaCP(char *cp)
+{
+    if ((cp[0] < '0' || cp[0] > '9') && (cp[1] < '0' || cp[1] > '9') && (cp[2] < '0' || cp[2] > '9') && (cp[3] < '0' || cp[3] > '9'))
+    {
+        return 0;
+    }
+    else if (cp[4] != '.')
+    {
+        return 0;
+    }
+    else if ((cp[5] < '0' || cp[5] > '9') && (cp[6] < '0' || cp[6] > '9'))
+    {
+        return 0;
+    }
+    
+
+    return 1;
+}
+
+/*Verifica se os caracteres da data são válidos*/
+int verificaData(char *data)
+{
+    int dias;
+
+    if (data[6] == '1')     //Se a dezena do ano for 1, entra no if
+    {
+        if (data[7] < '6')      //Se for um ano menor que 2016, não é um ano válido
+        {
+            return 0;
+        }
+        else if (data[7] < '6' || data[7] > '9')        //Verifica se os caracteres do ano são validos
+        {
+            return 0;
+        }
+    }
+
+    if (data[2] != '/' || data[5] != '/')       //Se os delimitadores da data não for '/', não é uma data válida
+    {
+        return 0;
+    }
+
+    dias = verificaMes(data);       //Descobrindo quantos dias tem o mês
+
+    if (dias == 0)     //Se não for um mês válido, retorna 0
+    {
+        return 0;
+    }
+    else if (dias == 30)        //Se for um mês com 30 dias entra no if
+    {
+        if (data[0] < '0' || data[0] > '3')     //Verifica se os caracteres da dezena são válidos
+        {
+            return 0;
+        }
+        else if (data[0] == '3' && data[1] != '0')      //Se a dezena for 3, verifica se a unidade é 0
+        {
+            return 0;
+        }
+        else if (data[1] < '0' || data[1] > '9')        //Verifica se os caracteres da unidade do dia são válidos
+        {
+            return 0;
+        }
+    }
+    else if (dias == 31)        //Se for um mês com 31 dias entra no if
+    {
+        if (data[0] < '0' || data[0] > '3')     //Verifica se os caracteres da dezena são válidos
+        {
+            return 0;
+        }
+        else if (data[0] == '3' && data[1] != '0' && data[1] != '1')        //Se a dezena for 3, verifica se a unidade é 0 ou 1
+        {
+            return 0;
+        }
+        else if (data[1] < '0' || data[1] > '9')        //Verifica se os caracteres da unidade do dia são válidos
+        {
+            return 0;
+        }
+    }
+    else if (dias == 29)        //Se for um mês com 29 dias entra no if
+    {
+        if (data[0] < '0' || data[0] > '2')     //Verifica se os caracteres da dezena são válidos
+        {
+            return 0;
+        }
+        else if (data[1] < '0' || data[1] > '9')        //Verifica se os caracteres da unidade do dia são válidos
+        {
+            return 0;
+        }
+    }
+    else if (dias == 28)        //Se for um mês com 29 dias entra no if
+    {
+        if (data[0] < '0' || data[0] > '2')     //Verifica se os caracteres da dezena são válidos
+        {
+            return 0;
+        }
+        else if (data[0] == '2')        //Se a dezena for 2, entra no if
+        {
+            if (data[1] < '0' || data[1] > '8')         //Verifica se os caracteres da unidade do dia são válidos
+            {
+                return 0;
+            }                
+        }
+        else if (data[1] < '0' || data[1] > '9')        //Verifica se os caracteres da unidade do dia são válidos
+        {
+            return 0;
+        }
+    }
+
+    return 1;       //Retorna que é uma data válida
+}
+
+/*Verifica o mês*/
+int verificaMes(char *data)
+{
+    if (data[3] < '0' || data[3] > '1')     //Verifica se os caracteres da dezena do mês são válidos
+    {
+        return 0;
+    }
+    else if (data[4] < '0' || data[4] > '9')        //Verifica se os caracteres da unidade do mês são válidos
+    {
+        return 0;
+    }
+
+    if (data[3] == '0')     //Se for um mês entre Janeiro e Setembro, entra no if
+    {
+        if (data[4] == '1' || data[4] == '3' || data[4] == '5' || data[4] == '7' || data[4] == '8')     //Verifica se é um mês com 31 dias
+        {
+            return 31;
+        }
+        else if (data[4] == '2')        //Se for Fevereiro, entra no if
+        {
+
+            if (verificaBissexto(data))        //Se for um ano bissexto, fevereiro terá 29 dias
+            {                
+                return 29;
+            }
+
+            return 28;      //Caso contrário terá 28 dias           
+        }
+        
+        return 30;      //Para qualquer outro mês entre 1 e 9 será um mês com 30 dias 
+    }
+
+    if (data[3] == '1' && data[4] == '1')       //Se for um mês mês de Novembro, terá 30 dias
+    {
+        return 30;
+    }
+    else if (data[3] == '1' && (data[4] == '0' || data[4] == '2'))      //Se for Outubro ou dezembro terá 31 dias
+    {
+        return 31;
+    }
+
+    return 0;       //Caso contrário é um mês inválido
+}
+
+/*Verifica se o ano é bissexto*/
+int verificaBissexto(char *data)
+{
+    int i;
+    char ano[3];
+
+    ano[0] = data[6];       //Copia os caracteres que delimitam o ano para a string ano
+    ano[1] = data[7];
+    ano[2] = '\0';
+    i = atoi(ano);       //Transforma a string ano em número
+
+    if (i % 4 == 0)           //Um ano divísivel por 4 é bissexto
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*Verifica se os caracteres da hora são válidos*/
+int verificaHora(char *hora)
+{
+    if (hora[0] < '0' || hora[0] > '2')     //Verifica se o caractere da dezena das horas é válido
+    {
+        return 0;
+    }
+    else if (hora[0] == '2' && (hora[1] < '0' || hora[1] > '3'))        //Verifica se o caractere da unidade das horas é válido
+    {
+        return 0;
+    }
+    else if(hora[2] != ':')     //Verifica se o caractere separador de horas e minutos é válido
+    {
+        return 0;
+    }
+    else if (hora[3] < '0' || hora[3] > '5')        //Verifica se o caractere da dezena dos minutos é válido
+    {
+        return 0;
+    }
+    else if (hora[4] < '0' || hora[4] > '9')        //Verifica se o caractere da unidade dos minutos é válido
+    {
+        return 0;
+    }
+    return 1;
+}
+
+/*Verifica se o nome do treinador é válido*/
+int verificaTreinador(char *treinador)
+{
+    int i = 0;
+
+    while (treinador[i] != '\0')    //Verifica se o nome do treinador é válido, ou seja se possui apenas números ou letras
+    {
+        if ((treinador[i] < 'A' || treinador[i] > 'Z') && (treinador[i] < 'a' || treinador[i] > 'z') && (treinador[i] < '0' || treinador[i] > '9'))
+        {
+            return 0;
+        }
+
+        i++;
+    }
+
+    i = 0;
+
+    while (treinador[i] != '\0')        //Enquanto não chegar no final da string, deixa todas as letras maiusculas
+    {
+        treinador[i] = toupper(treinador[i]);
+        i++;
+    }
+
+    return 1;
+}
+
+/*Verifica se o nivel do treinador é válido*/
+int verificaNivel(char *nivel)
+{
+    if (nivel[0] == '0' && nivel[1] == '0' && nivel[2] == '0')
+    {
+        return 0;
+    }
+    else if (nivel[0] == '0')      //Verifica se o nível é válido
+    {
+        if ((nivel[1] < '0' || nivel[1] > '9') && (nivel[2] < '0' && nivel[2] > '9'))
+        {
+            return 0;
+        }
+
+        return 1;
+    }
+    else if (nivel[0] == '1' && nivel[1] == '0' && nivel[2] == '0')
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*Verifica se o nome da equipe é válido*/
+int verificaEquipe(char *equipe)
+{
+    int i = 0;
+
+    if (!verificaNome(equipe))      //Verifica se o nome da equipe possui algum caracteve inválido
+    {
+        return 0;
+    }
+
+    while (equipe[i] != '\0')       //Enquanto não chegar no final da string, deixa todas as letras maiusculas
+    {
+        equipe[i] = toupper(equipe[i]);
+        i++;
+    }
+
+    if (!strcmp("VALOR", equipe))       //Verifica se o nome da equipe é válido
+    {
+        return 1;
+    }
+    else if (!strcmp("INSTINCT", equipe))
+    {
+        return 1;
+    }
+    else if (!strcmp("MYSTIC", equipe))
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*Função que gera a chave primária de um novo registro*/
+void geraChavePrimaria(Pokemon *info)
+{
+    info->codigo[0] = info->nomeEquipe[0];
+    info->codigo[1] = info->treinador[0];
+    info->codigo[2] = info->nomePokemon[0];
+    info->codigo[3] = info->nomePokemon[1];
+    info->codigo[4] = info->data[0];
+    info->codigo[5] = info->data[1];
+    info->codigo[6] = info->data[3];
+    info->codigo[7] = info->data[4];
+    info->codigo[8] = info->hora[0];
+    info->codigo[9] = info->hora[1];
+    info->codigo[10] = info->hora[3];
+    info->codigo[11] = info->hora[4];
+    info->codigo[12] = '\0';
+}
+
+/*Função que verifica se a chave primaria gerada já existe*/
+int verificaChave(Pokemon info, Indice *vet, int tam)
+{
+    int pos;
+
+    pos = buscaChavePrimaria(info.codigo, vet, 0, tam);     //Busca a chave primaria no vetor
+
+    if (pos == -1)
+    {
+        return 1;
+    }
+
+    return 0;
+}
 
 
 /* ==========================================================================
